96dbe6cccb45ab0297178ac6852343fa13913fc9
Jason Scott Timmerman <Jason.Timmerman@GMail.com>
2015-07-20 11:52:22 -0600

    Main GUI for recording screen is set up.

--- Pitch Perfect/ViewController.swift
+++ Pitch Perfect/ViewController.swift
@@ -10,6 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var recordButton: UIButton!
+    
+    @IBOutlet weak var recordingLabel: UILabel!
+    
+    @IBOutlet weak var stopButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +26,19 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func recordAudio(sender: UIButton) {
+        //TODO: Begin recording the audio clip
+        recordingLabel.hidden = false
+        stopButton.hidden = false
+        recordButton.enabled = false
+    }
 
+    @IBAction func stopRecording(sender: UIButton) {
+        //TODO: Stop recording audio
+        recordingLabel.hidden = true
+        stopButton.hidden = true
+        recordButton.enabled = true
+    
+    }
 }
 
