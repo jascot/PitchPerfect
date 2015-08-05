5cc6869d91f8079edfa17a1c3b2d54259d1d93d7
Jason Scott Timmerman <Jason.Timmerman@GMail.com>
2015-08-05 15:44:25 -0600

    Pause button added to record screen and pause and resume recording now working.

--- Pitch Perfect/RecordSoundsViewController.swift
+++ Pitch Perfect/RecordSoundsViewController.swift
@@ -13,24 +13,29 @@ class RecordSoundsViewController: UIViewController, AVAudioRecorderDelegate {
     
     var recorder: AVAudioRecorder!
     var audio: RecordedAudio!
+    var paused: Bool = false
     
     @IBOutlet weak var recordButton: UIButton!
     @IBOutlet weak var recordingLabel: UILabel!
     @IBOutlet weak var stopButton: UIButton!
+    @IBOutlet weak var pauseButton: UIButton!
     
     override func viewWillAppear(animated: Bool) {readyToRecord()}
 
     @IBAction func recordAudio(sender: UIButton) {
         recordingLabel.text = "Recording in Progress"
         stopButton.hidden = false
+        pauseButton.hidden = false
         recordButton.enabled = false
         
-        do {try AVAudioSession.sharedInstance().setCategory(AVAudioSessionCategoryPlayAndRecord)} catch {}
+        if(!paused) {
+            do {try AVAudioSession.sharedInstance().setCategory(AVAudioSessionCategoryPlayAndRecord)} catch {}
         
-        do {try recorder = AVAudioRecorder(URL: NSURL.fileURLWithPathComponents([NSSearchPathForDirectoriesInDomains(NSSearchPathDirectory.DocumentDirectory, NSSearchPathDomainMask.UserDomainMask, true)[0] as String, "recording.wav"])!, settings: [AVSampleRateKey: 44100.0])} catch {}
-        recorder.meteringEnabled = true
-        recorder.delegate = self
-        recorder.prepareToRecord()
+            do {try recorder = AVAudioRecorder(URL: NSURL.fileURLWithPathComponents([NSSearchPathForDirectoriesInDomains(NSSearchPathDirectory.DocumentDirectory, NSSearchPathDomainMask.UserDomainMask, true)[0] as String, "recording.wav"])!, settings: [AVSampleRateKey: 44100.0])} catch {}
+            recorder.meteringEnabled = true
+            recorder.delegate = self
+            recorder.prepareToRecord()
+        }
         recorder.record()
     }
     
@@ -39,6 +44,15 @@ class RecordSoundsViewController: UIViewController, AVAudioRecorderDelegate {
         do {try AVAudioSession.sharedInstance().setActive(false)} catch {}
     }
     
+    @IBAction func pauseRecording(sender: UIButton) {
+        recorder.pause()
+        recordingLabel.text = "Tap Mic to Continue"
+        stopButton.hidden = true
+        pauseButton.hidden = true
+        recordButton.enabled = true
+        paused = true
+    }
+
     func audioRecorderDidFinishRecording(recorder: AVAudioRecorder, successfully flag: Bool) {
         if(flag) {
             audio = RecordedAudio(url: recorder.url, tit: recorder.url.lastPathComponent!)
@@ -55,7 +69,9 @@ class RecordSoundsViewController: UIViewController, AVAudioRecorderDelegate {
     func readyToRecord() {
         recordingLabel.text = "Tap Mic to Record"
         stopButton.hidden = true
+        pauseButton.hidden = true
         recordButton.enabled = true
+        paused = false
     }
 }
 
